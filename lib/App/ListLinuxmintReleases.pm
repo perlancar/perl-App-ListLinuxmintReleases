c68df2ffdcf9f737b4f220053b2ee9add47df65c
perlancar (@pc-office) <perlancar@gmail.com>
2015-10-06 19:41:11 +0700

    Initial commit

---
+++ lib/App/ListLinuxmintReleases.pm
@@ -0,0 +1,13 @@
+package App::ListLinuxmintReleases;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: List LinuxMint releases (CLI)
+
+=head1 DESCRIPTION
+
+See the included script L<list-linuxmint-releases>.
+
+=cut
